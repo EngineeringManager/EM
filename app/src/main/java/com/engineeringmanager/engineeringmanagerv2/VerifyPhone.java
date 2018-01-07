30b16c28c56bbd906d2244854139d93ce028406b
Aditya Khandare <khandareaditya97@gmail.com>
2018-01-07 18:59:35 +0530

    initial commit

---
+++ app/src/main/java/com/engineeringmanager/engineeringmanagerv2/VerifyPhone.java
@@ -0,0 +1,38 @@
+package com.engineeringmanager.engineeringmanagerv2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class VerifyPhone extends AppCompatActivity {
+
+    private final int REQUEST_LOGIN=1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_phone);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if(auth.getCurrentUser() !=null)
+        {
+            if(!FirebaseAuth.getInstance().getCurrentUser().getEmail()
+                    .isEmpty())
+            {
+                startActivity(new Intent(this,SignIn.class)
+                .putExtra("phone",FirebaseAuth.getInstance()
+                        .getCurrentUser().getPhoneNumber().isEmpty())
+                );
+                finish();
+            }
+            else
+            {
+                /*startActivityForResult(AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailabeProviders(new AuthUI.IdpConfig
+                        .Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build()));*/
+            }
+        }
+    }
+}
