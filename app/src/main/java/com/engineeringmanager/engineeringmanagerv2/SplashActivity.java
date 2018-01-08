f4808fea476f16eb5123881adb0706a9635ad591
Aditya Khandare <khandareaditya97@gmail.com>
2018-01-09 00:24:32 +0530

    Commit with splash activity done. Verify Phone is commented App compat Error solved

--- app/src/main/java/com/engineeringmanager/engineeringmanagerv2/SplashActivity.java
+++ app/src/main/java/com/engineeringmanager/engineeringmanagerv2/SplashActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
+
 public class SplashActivity extends AppCompatActivity {
 
     private  static int SPLASH_TIME_OUT=4000;
